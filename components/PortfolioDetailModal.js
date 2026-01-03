0f744aead74d8b21d616ad2c9a64a367c3a989ee
hwangtab <hwangtab@gmail.com>
2026-01-04 01:01:00 +0900

    fix: remove unused AnimatePresence import from PortfolioDetailModal

--- components/PortfolioDetailModal.js
+++ components/PortfolioDetailModal.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useCallback } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { X, Share2, ExternalLink } from 'lucide-react';
 import ResponsiveImage from './ResponsiveImage';
 import { categories } from '../data/portfolio';
