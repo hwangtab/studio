2de0f92a7fccd9be91d3b735d569ca3d696c20f8
hwangtab <hwangtab@gmail.com>
2026-01-30 16:53:25 +0900

    style(ui): calibrate hero vertical center with pt-32 pb-12

--- components/common/ImageHero.js
+++ components/common/ImageHero.js
@@ -26,7 +26,7 @@ const ImageHero = ({
 
   return (
     <section
-      className={`relative overflow-hidden ${minHeight} flex flex-col justify-center -mt-20 pt-48 pb-20 ${className}`}
+      className={`relative overflow-hidden ${minHeight} flex flex-col justify-center -mt-20 pt-32 pb-12 ${className}`}
     >
       {/* 배경 이미지 (Ken Burns Effect + Parallax) */}
       <motion.div
