97eea6fd65fc74e2d31eb77d9967968b508d0c80
황경하 <hwang-gyeongha@Macmini-2676.local>
2026-02-14 01:34:06 +0900

    AudioPlayer progress ref 타입을 null 가능 타입으로 정정

--- components/AudioPlayer/ProgressBar.tsx
+++ components/AudioPlayer/ProgressBar.tsx
@@ -4,7 +4,7 @@ interface ProgressBarProps {
     currentTime: number;
     duration: number;
     progress: number;
-    progressBarRef: React.RefObject<HTMLInputElement>;
+    progressBarRef: React.MutableRefObject<HTMLInputElement | null>;
     onChangeRange: () => void;
     formatTime: (time: number) => string;
 }
